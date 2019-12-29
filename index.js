544e12f33406908ab88360da9e44de5fa6477a15
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2019-12-29 23:48:51 +0530

    updated index.js
    code for routing in home server.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs= require('fs');
 const http= require('http');
+const url= require('url');
 ////////////////////////////////////////////////////////////////////////////////
 
 //Blocking synchronous  way 
@@ -26,12 +27,31 @@ console.log("will read file!");
 //SERVER
  
 const server=http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url);  
+    const pathname = req.url;
+    if(pathname === '/over'|| pathname==='/')
+    {
+        res.end('this is the overview');
+
+    } 
+
+    else if(pathname==='/product'){
+        res.end('product ');
+    }
+    else{
+        res.writeHead('404',{
+            'Conent-type ':'text/html'
+        });
+        res.end('<h1> page not found!</h1>');
+    }
+   
 });
 
 server.listen(8000,'127.0.0.1',()=>{
     console.log('server has been started');
 })
+
+
 
 
 
