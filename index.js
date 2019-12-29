8ea4217bdbe7581bc024a4b15a41936ff5d5f6ea
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2019-12-29 21:45:14 +0530

    Updated index.js
    created a local host.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs= require('fs');
+const http= require('http');
+////////////////////////////////////////////////////////////////////////////////
 
 //Blocking synchronous  way 
 //const textin = fs.readFileSync( './txt/input.txt','utf8');
@@ -9,12 +11,45 @@ const fs= require('fs');
 // console.log("file Written!");
   
  // Non-Blocking Asynchronous way.
-
-fs.readFile('.txt/start.txt','utf-8', (err,data1)=>{
+//
+/*fs.readFile('.txt/start.txt','utf-8', (err,data1)=>{
      if(err) return console.log('error');
     fs.writeFile(`.txt/${data1}.txt`,'utf-8', (err,data2)=>{
     console.log(data2);
     });
 });
 console.log("will read file!");
+*///
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//SERVER
+ 
+const server=http.createServer((req,res)=>{
+    res.end('hello from the server');
+
+});
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('server has been started');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
